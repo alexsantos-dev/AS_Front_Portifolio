c5939ee1cca2849654e005cbef9095d6cc93a05c
Alex Santos <alexsantos.web.developer@gmail.com>
2023-11-28 13:41:43 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <header>
+        <nav>
+          <ul>
+            <li>ABCDEFG</li>
+            <li>BCDEFGH</li>
+            <li>CDEFGHI</li>
+          </ul>
+        </nav>
+      </header>
+      <main>
+        <h1>Olá seja muito bem-vindo ao meu portifólio</h1>
+        <h2>Sobre mim</h2>
+        <h3>Habilidades</h3>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit, eos
+          voluptates voluptate ullam voluptas repellat iure unde ipsum itaque
+          veniam sit eius laudantium provident distinctio dolor quibusdam
+          dolorum cupiditate? Sapiente.
+        </p>
+        <br />
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit, eos
+          voluptates voluptate ullam voluptas repellat iure unde ipsum itaque
+          veniam sit eius laudantium provident distinctio dolor quibusdam
+          dolorum cupiditate? Sapiente.
+        </p>
+        <br />
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Velit, eos
+          voluptates voluptate ullam voluptas repellat iure unde ipsum itaque
+          veniam sit eius laudantium provident distinctio dolor quibusdam
+          dolorum cupiditate? Sapiente.
+        </p>
+      </main>
+    </>
+  );
+}
+
+export default App;
