fad2d44648a707dbd72132022ac4922a7993c096
Alex Santos <alexsantos.web.developer@gmail.com>
2023-11-29 20:18:24 -0300

    adicioando estilos no navbar e responsividade

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,25 +5,27 @@ function Navbar() {
     <nav>
       <ul className="contato">
         <li>
-          <a href="#">
-            <img src="../public/linkedin.svg" alt="logo" />
+          <a
+            href="https://www.linkedin.com/in/alex-santos-b020b5296"
+            target="blank">
+            <img src="/linkedin.svg" alt="linkedIn" title="LinkedIn" />
           </a>
         </li>
         <li>
-          <a href="#">
-            <img src="../public/github.svg" alt="logo" />
+          <a href="https://github.com/alexsantos-dev" target="blank">
+            <img src="/github.svg" alt="github" title="Github" />
           </a>
         </li>
         <li>
-          <a href="#">
-            <img src="../public/whatsapp.svg" alt="logo" />
+          <a href="https://wa.me/+5598981630767" target="blank">
+            <img src="/whatsapp.svg" alt="whatsapp" title="Whatsapp" />
           </a>
         </li>
       </ul>
       <ul className="logo">
         <li>
           <a href="#">
-            <img src="../public/logo.svg" alt="logo" className="logoIcon" />
+            <img src="/logo.svg" alt="logo" className="logoIcon" />
           </a>
         </li>
       </ul>
