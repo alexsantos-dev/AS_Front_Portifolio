70442fc13a94ceadc8e245b41ea009f74b16d4e5
Alex Santos <alexsantos.web.developer@gmail.com>
2023-12-12 10:57:33 -0300

    ajuste no timeout do axios

--- src/services/projetos.service.js
+++ src/services/projetos.service.js
@@ -2,22 +2,27 @@ import axios from "axios";
 
 const baseURL = "https://api-portifolio-ohio-br.onrender.com";
 
+const axiosInstance = axios.create({
+    baseURL,
+    timeout: 10000,
+});
+
 export function getRecentes() {
-    return axios.get(`${baseURL}/projetos/recentes`);
+    return axiosInstance.get("/projetos/recentes");
 }
 
 export function getRelevantes() {
-    return axios.get(`${baseURL}/projetos/relevantes`);
+    return axiosInstance.get("/projetos/relevantes");
 }
 
 export function visualizar(projetoId) {
-    return axios.patch(`${baseURL}/projetos/visualizacoes/${projetoId}`);
+    return axiosInstance.patch(`/projetos/visualizacoes/${projetoId}`);
 }
 
 export function compartilhar(projetoId) {
-    return axios.patch(`${baseURL}/projetos/compartilhar/${projetoId}`);
+    return axiosInstance.patch(`/projetos/compartilhar/${projetoId}`);
 }
 
 export function acessarRep(projetoId) {
-    return axios.patch(`${baseURL}/projetos/acessRepositorio/${projetoId}`)
+    return axiosInstance.patch(`/projetos/acessRepositorio/${projetoId}`);
 }
