054f0f46dfa5534f81910a21f6b2fda073206b2a
Alex Santos <alexsantos.web.developer@gmail.com>
2023-12-12 18:53:26 -0300

    ajustando url à api na aws

--- src/services/projetos.service.js
+++ src/services/projetos.service.js
@@ -5,6 +5,14 @@ const baseURL = "http://18.231.155.174:3000";
 const axiosInstance = axios.create({
     baseURL,
     timeout: 40000,
+    withCredentials: true,
+});
+
+axiosInstance.interceptors.request.use(config => {
+    config.headers['Access-Control-Allow-Origin'] = 'https://seu-frontend-na-vercel.vercel.app'; // 
+    config.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE';
+    config.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization';
+    return config;
 });
 
 export function getRecentes() {
