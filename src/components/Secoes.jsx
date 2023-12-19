ea044b9e4792837b77bfc55b998612a77216359a
Δlex Santos <140655997+alexsantos-dev@users.noreply.github.com>
2023-12-19 12:01:16 -0300

    ajustando estilos

--- src/components/Secoes.jsx
+++ src/components/Secoes.jsx
@@ -67,7 +67,7 @@ export function Secoes() {
     <SecoesContainer>
       <div className="bg-nav-section">
         <section id="bem-vindo">
-          <h1>Bem vindo(a) ao meu portifólio!</h1>
+          <h1>Bem-vindo(a) ao meu portifólio!</h1>
         </section>
       </div>
       <section id="Sobre">
